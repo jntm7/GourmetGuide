358695e98204c7f7a888c0aab24815818ccfcbb6
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-07-17 18:01:11 -0600

    Initialization

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import json
 from config import EDMAM_API_ID, EDMAM_API_KEY
 
 class GetRecipe:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Gourmet Guide")
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = GetRecipe(root)
+    root.mainloop()
