c8748877e9aa226dca91711a662c4cdeb2e544c9
Jonathan <108718802+jntm7@users.noreply.github.com>
2024-07-17 18:15:52 -0600

    Create meal type dropdown

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ class GetRecipe:
 
 
     def dropdown_menus(self):
-        self.cuisine_label = tk.Label(self.root, text="Select Cuisine:")
+
+        # Cuisine Type
+        self.cuisine_label = tk.Label(self.root, text="Select Cuisine Type:")
         self.cuisine_label.pack()
         self.cuisine_var = tk.StringVar()
         self.cuisine_dropdown = ttk.Combobox(self.root, textvariable=self.cuisine_var)
@@ -34,8 +36,20 @@ class GetRecipe:
             "South American",
             "South East Asian",
         )
+        self.cuisine_dropdown.pack()
 
-
+        # Meal Type
+        self.meal_label = tk.Label(self.root, text="Select Meal Type:")
+        self.meal_label.pack()
+        self.meal_var = tk.StringVar()
+        self.meal_dropdown = ttk.Combobox(self.root, textvariable=self.meal_var)
+        self.meal_dropdown['values'] = (
+            "Breakfast",
+            "Lunch",
+            "Dinner",
+            "Snack",
+        )
+        self.meal_dropdown.pack()
 
 if __name__ == "__main__":
     root = tk.Tk()
